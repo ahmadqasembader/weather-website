79bc3dc77bc6756dbe50fa55f68ed05f25468424
Ahmad Bader <ahmabader245@gmail.com>
2020-11-23 18:20:02 +0200

    change the api keys

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.openweathermap.org/data/2.5/weather?units=metric&lat=' + latitude + '&lon=' + longitude + '&appid=2ad080b2e81d4af40532e7c549cb2c66'
+    const url = 'http://api.openweathermap.org/data/2.5/weather?units=metric&lat=' + latitude + '&lon=' + longitude + '&appid=dc7cfb6dc5dbb3a3feda9cf6a72a5e55'
 
     request( { url, json: true }, (error, {body}) => {
         if(error){
@@ -9,12 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.cod !== 200){
             callbacl('Location not found. Try again with different location please!', undefined)
         }else{
-            callback(undefined,/*
-                latitude: body.coord.lat,
-                longitude: body.coord.lon,
-                cityName: body.name,*/
-                'The weather in ' + body.name + ' is ' +  body.weather[0].description +  ' with temperature ' + body.main.temp
-            )
+            callback(undefined, 'The weather in ' + body.name + ' is ' +  body.weather[0].description +  ' with temperature ' + body.main.temp)
         }
     })
 }
